27653c64c5019ca4e73693ff247178e14c5acb59
Akuaamoako <Ekuaamoako11@gmail.com>
2023-02-22 05:11:43 -0800

    add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+   * add - adds two integers
+    * @a: first integer to add
+     * @b: second integer to add
+      *
+       * Return: sum of a and b
+        */
+int add(int a, int b)
+{
+	    return (a + b);
+}
