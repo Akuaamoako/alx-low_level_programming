664f701e79115b5229c89aeac25f9bf2b767ca1d
Akuaamoako <Ekuaamoako11@gmail.com>
2023-02-27 03:50:23 -0800

    rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: The string to be reversed
+ */
+void rev_strong(char *s)
+{
+	int len = 0, i = 0;
+	char temp;
+
+	while (s[i++])
+		len++;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i = 1];
+		s[len - i - 1] = temp;
+	}
+}
