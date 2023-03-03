a97a59abae5222aa6ed0e32cb8f0146914e80f14
Akuaamoako <ekuaamoako11@gmail.com>
2023-03-03 05:03:30 -0800

    4_rev_array.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,13 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-int _strcmp(char *s1, char *s2);
+#include <stdio.h>
+
+void reverse_array(int *a, int n);
 
 #endif /* MAIN_H */
