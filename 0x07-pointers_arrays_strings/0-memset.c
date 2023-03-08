fcdceb58acb8aaf2daeddf42e174bcf0017e7c2d
Akuaamoako <ekuaamoako11@gmail.com>
2023-03-08 02:58:51 -0800

    0memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int num = 0;
+	printf("The original value of num is: %d\n", num);
+
+	// Perform some operation to mend the value of num
+	num = 0;
+
+	printf("The mended value of num is: %d\n", num);
+	return 0;
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Macros */
+#define MAX_LENGTH 100
+
+/* Function Declarations */
+int add_numbers(int a, int b);
+void print_message(char *message);
+
+endif /* MAIN_H */
